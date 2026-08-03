6e13a0b9f0344f1cf201f24ff1d7dd1ff5b5d49a
William-nyarash <owetywilliamnyaranga@gmail.com>
2026-08-03 22:39:51 +0300

    refactor code

--- models/index.js
+++ models/index.js
@@ -5,17 +5,6 @@ const User = require("./user")
 User.hasMany(Blog, { foreignKey: 'user_id' });
 Blog.belongsTo(User, { foreignKey: 'user_id' });
 
-const syncDatabse = async () => {
-try{
-
-  await Blog.sync({ alter: true})
-  await User.sync({ alter: true})
-} catch (error) {
-  console.error("error: ", error.message)
-}
-}
-
-syncDatabse()
 
 module.exports = {
   Blog, User
