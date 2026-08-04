d25515b4cd2258c1c4b2055556995b389460cfb8
William-nyarash <owetywilliamnyaranga@gmail.com>
2026-08-04 09:42:56 +0300

    refactor the code

--- util/db.js
+++ util/db.js
@@ -1,37 +1,35 @@
-const  Sequelize = require("sequelize")
-const { DATABASE_URL, TEST_DATABASE_URL} = require("./config")
+const { Sequelize } = require("sequelize");
 
-require("dotenv").config()
-const { TESTING } = process.env
-const DATABASE  =  TESTING === "true" ? TEST_DATABASE_URL : DATABASE_URL;
+const { DATABASE_URL, TEST_DATABASE_URL } = require("./config");
+require("dotenv").config();
 
-const sequelize =  new Sequelize( DATABASE, {
-     dialectOptions: {
+const { TESTING } = process.env;
+const DATABASE = TESTING === "true" ? TEST_DATABASE_URL : DATABASE_URL;
+
+const sequelize = new Sequelize(DATABASE, {
+  dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false
-    }
+      rejectUnauthorized: false,
+    },
   },
-})
-
+});
 
 const connectToDatabase = async () => {
-    try{
-        await sequelize.authenticate()
-        if( process.env.TESTING  === "true"){
-          await sequelize.sync({alert: true})
-          console.log("Database synchronized")
-        }else{
-          console.log("connected to the database")
-        }
-        
-    } catch (errorO) {
-        console.error("Failed to connect to the databse")
-        return process.exit(1)
+  try {
+    await sequelize.authenticate();
+    
+    if (process.env.TESTING === "true") {
+      await sequelize.sync({ alter: true }); 
+      console.log("Database synchronized");
+    } else {
+      console.log("Connected to the database");
     }
-    return  null 
+  } catch (error) {
+    console.error("Failed to connect to the database:", error);
+    return process.exit(1);
+  }
+  return null;
+};
 
-}
-
-
-module.exports = {connectToDatabase,sequelize}
+module.exports = { connectToDatabase, sequelize };
