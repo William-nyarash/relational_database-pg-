b2e4d3be62e6a639a3bcfa5a52b0dcb217ba2325
William-nyarash <owetywilliamnyaranga@gmail.com>
2026-08-20 20:12:51 +0300

    modify return value

--- controllers/status.controller.js
+++ controllers/status.controller.js
@@ -2,10 +2,7 @@
 const router =require('express').Router()
 
 router.get("/", (req, res, next) => {
-        const data = {
-            massege:"All systems are up and running"
-        }
-        return res.status(200).json(data)
+        return res.status(200).json({status: 'ok' })
 })
 
 module.exports = router
